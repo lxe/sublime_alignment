6e59984785ccfeadd59021950e837b08d907897f
lxe <lxe@lxe.co>
2013-06-12 14:44:53 -0400

    Added ST3 support (while removing ST2 support)

--- Alignment.py
+++ Alignment.py
@@ -5,14 +5,8 @@ import math
 import os
 import sys
 
-# This is necessary due to load order of packages in Sublime Text 2
-sys.path.append(os.path.join(sublime.packages_path(), 'Default'))
-indentation = __import__('indentation')
-reload(indentation)
-del sys.path[-1]
-
-normed_rowcol = indentation.line_and_normed_pt
-
+import Default.indentation
+normed_rowcol = Default.indentation.line_and_normed_pt
 
 def convert_to_mid_line_tabs(view, edit, tab_size, pt, length):
     spaces_end = pt + length
